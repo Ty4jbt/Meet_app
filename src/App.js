5a22db457d74b2f9db8783d362d9ce678fdbbf6e
Ty4jbt <tyaboe@gmail.com>
2021-08-27 16:57:21 -0700

    refactored to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import NumberofEvents from './NumberofEvents';
+import NumberOfEvents from './NumberOfEvents';
+// import mockData from './mock-data';
+import { getEvents, extractLocations } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-      <NumberofEvents />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32,
+    currentCity: 'all',
+  }
+
+  updateEvents = (location, numberOfEvents) => {
+    getEvents().then((events) => {
+      const locationEvents = 
+        location === 'all'
+          ? events.slice(0, numberOfEvents)
+          : events.filter((event) => event.location === location);
+      if (this.mounted) {
+        this.setState({
+          events: locationEvents.slice(0, numberOfEvents),
+          currentCity: location,
+          //locations: [location],
+        });
+      }
+    });
+  };
+
+  updateNumberOfEvents(eventNumber) {
+    this.setState({ numberOfEvents: eventNumber });
+    const { currentCity } = this.state;
+    this.updateEvents(currentCity, eventNumber);
+  }
+
+  componentDidMount() {
+    const { numberOfEvents } = this.state;
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({
+          events: events.slice(0, numberOfEvents),
+          locations: extractLocations(events),
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NumberOfEvents
+          updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)}
+        />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+          numberOfEvents={this.state.numberOfEvents}
+        />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
