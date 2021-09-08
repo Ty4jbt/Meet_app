770f4bea6d2dc78b52f16372d0a38a41146436d1
Ty4jbt <tyaboe@gmail.com>
2021-09-08 05:34:35 -0700

    added additional acceptance tests

--- src/NumberofEvents.js
+++ src/NumberofEvents.js
@@ -3,16 +3,29 @@ import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
 
+  state = {
+    numberOfEvents: 32
+  }
+
   handleChange = (event) => {
     const value = event.target.value;
-    this.props.updateNumberOfEvents(value);
-  }
+    if ( value < 1 || value > 32 ) {
+      return this.setState({
+        numberOfEvents: ''
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value
+      });
+    }
+  };
 
   render() {
     return (
       <div className="numberOfEvents">
         <label htmlFor="fname">Events per page:
           <input 
+            className='event-number-input'
             type="text"
             id="number" 
             value={this.props.numberOfEventsShown} 
