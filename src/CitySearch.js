19723908a46b925303e91b0739cc0e811abec402
Ty4jbt <tyaboe@gmail.com>
2021-09-10 19:42:25 -0700

    added an alert to ask  for another try

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
         query: '',
         suggestions: [],
-        showSuggestions: false
+        showSuggestions: false,
+        infoText: ''
     }
     
     // updateList() {
@@ -16,19 +18,30 @@ class CitySearch extends Component {
 
     handleInputChanged = (event) => {
         const value = event.target.value;
+        this.setState({ showSuggestions: true });
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-            query: value,
-            suggestions,
-        });
-    }
+        if (suggestions.length === 0) {
+            this.setState({
+                query: value,
+                infoText: 'We can not find the city you are looking for. Please try another city',
+            });
+        } else {
+            this.setState({
+                query: value,
+                suggestions,
+                infoText: ''
+            });
+        }
+    };
 
     handleItemClicked = (suggestion) => {
         this.setState({
           query: suggestion,
-          showSuggestions: false
+          suggestions: [],
+          showSuggestions: false,
+          infoText: ''
         });
         this.props.updateEvents(suggestion);
     }
@@ -36,6 +49,7 @@ class CitySearch extends Component {
     render() {
         return (
             <div className='CitySearch' >
+                <InfoAlert text={this.state.infoText} />
                 <input
                     type="text"
                     className='city'
@@ -51,7 +65,9 @@ class CitySearch extends Component {
                             onClick={() => this.handleItemClicked(suggestion)}
                         >{suggestion}</li>
                     ))}
-                    <li onClick={() => this.handleItemClicked('all')}>
+                    <li 
+                        key='All Locations'
+                        onClick={() => this.handleItemClicked('all')}>
                         <b>See all cities</b>
                     </li>
                 </ul>
